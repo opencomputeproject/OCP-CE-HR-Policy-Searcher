f2ec3201fea38e9de0a6cdf526258ed1eb8ace25
valdemarLaptop <valdemar.jeirud@gmail.com>
2026-04-13 12:15:27 +0200

    Replaced original homepage with an AI suggested first component, to have something to start with.

---
+++ frontend/src/components/PolicyList.js
@@ -0,0 +1,21 @@
+import React, { useEffect, useState } from 'react';
+
+function PolicyList() {
+  const [policies, setPolicies] = useState([]);
+
+  useEffect(() => {
+    fetch('/api/policies')
+      .then((res) => res.json())
+      .then(setPolicies)
+      .catch(console.error);
+  }, []);
+
+  return (
+    <div>
+      <h2>Policies</h2>
+      <pre>{JSON.stringify(policies, null, 2)}</pre>
+    </div>
+  );
+}
+
+export default PolicyList;
